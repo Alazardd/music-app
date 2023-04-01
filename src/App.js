3c63715a857436a9ab9a197778ecc986e491e85e
Alazar dinberu <alazardinberu64@gmail.com>
2023-04-01 20:47:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import SongList from "./components/SongList";
+import SongForm from "./components/SongForm";
+import Statistics from "./components/Statistics";
+import Filter from "./components/Filter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Filter />
+        <SongForm />
+        <SongList />
+        <Statistics />
+      </div>
+    </Provider>
   );
 }
 
